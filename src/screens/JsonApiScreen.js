1e7d3985d7dd1beba8134d53162057f6e288e9b0
valvic6 <valvic6@192-168-0-104.rdsnet.ro>
2020-11-04 14:45:14 +0200

    implemented api screen with data from API

--- src/screens/JsonApiScreen.js
+++ src/screens/JsonApiScreen.js
@@ -1,31 +1,50 @@
-import React, { useState } from "react";
-import { Text, StyleSheet, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { Text, StyleSheet, View, Image, ScrollView } from "react-native";
 import { Button } from 'react-native-elements';
+import api from '../api/api';
 
 const JsonApiScreen = () => {
   const [results, setResults] = useState([]);
 
   const getData = async () => {
     try {
-      const res = await fetch('https://jsonplaceholder.typicode.com/photos');
-      const json = await res.json();
-      setResults(json);
+      const res = await api.get('/photos');
+      setResults(res.data);
     } catch(err) {
-      console.error(err);
+      console.log(err);
     }
+  }
+
+  const src = results && results[0] && results[0].thumbnailUrl;
+  console.log('src', src);
+
+  const renderData = () => {
+    return results.map((result, index) => {
+      if(index > 500) {
+        return null;
+      }
+      return (
+      <View key={index} >
+        <Image style={{width: 200, height: 200}} source={{uri: result.thumbnailUrl}} />
+      </View>)
+    })
   }
 
   return (
     <View style={styles.jsonApiScreenWrapperStyle}>
       <Button title='Get JsonApi data' onPress={() => getData()} />
       <Text>{results.length}</Text>
+      <Text>{results && results[0] && results[0].id}</Text>
+      <ScrollView>
+        {renderData()}
+      </ScrollView>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   jsonApiScreenWrapperStyle: {
-    
+    flex: 1
   }
 });
 
